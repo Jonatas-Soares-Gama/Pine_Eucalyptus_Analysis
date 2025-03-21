20ad94d1ac9869facee7c30b6f8e2e04146ce562
Jonatas-Soares-Gama <jonatassoaresdagama@gmail.com>
2025-03-20 23:22:22 -0300

    Convertendo para float: valor_corrigido_IPCA, valor_medio_corrigido_IPCA

--- src/analysis/comparative_analysis.py
+++ src/analysis/comparative_analysis.py
@@ -11,11 +11,45 @@ df.columns.to_list()
 #%%
 
 df['Grupo'].unique()
+
 #%%
 
-group_type = df[['UF', 'Ano', 'Especie', 'Grupo', 'Uso', 'Quantidade', 'Valor_Corrigido_IPCA_Mil_R$', 'Preco_Medio_Corrigido_IPCA']]
+def limpar_valor(valor):
+    """
+    Limpa e converte valores monetários em string para float.
+    Remove separadores de milhar (pontos) e converte vírgulas para pontos decimais.
+    """
+    if isinstance(valor, str):
+        # Remove todos os pontos (separadores de milhar)
+        valor = valor.replace('.', '')
+        # Substitui vírgula por ponto (separador decimal)
+        valor = valor.replace(',', '.')
+        try:
+            return float(valor) # Converte o valor para float
+        except ValueError:  # Se não for possível converter, retorna None
+            print(f"Não foi possível converter o valor: {valor}")   
+            return None
+    return valor   
+#%%
 
-group_type.head()
+comparative_analysis = df[['UF', 'Ano', 'Especie', 'Grupo', 'Uso', 'Quantidade', 'Valor_Corrigido_IPCA_Mil_R$', 'Preco_Medio_Corrigido_IPCA']]
+
+comparative_analysis.head()
+
+def convert_to_float(df):
+
+    df = df.copy()
+
+    df['Valor_Corrigido_IPCA_Mil_R$'] = df['Valor_Corrigido_IPCA_Mil_R$'].apply(limpar_valor)
+    df['Preco_Medio_Corrigido_IPCA'] = df['Preco_Medio_Corrigido_IPCA'].apply(limpar_valor)
+
+    return df
+
+comparative_analysis = convert_to_float(comparative_analysis)
+
+comparative_analysis.head()
+
+# %%
 
 
 
